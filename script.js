936869c4f1a99e47198c77f0fb5586c4fad5c551
Sidney Lopes <sidney.silvalopes@gmail.com>
2024-05-23 18:10:40 -0300

    Palindromo e arrayMaxMin

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
 function isPalindrome(string) {
-    let minusculo = string.toLowerCase().replace();
-    let texto = string.length;
-  
-    for (i = 0; i < texto; i++) {
-      if (minusculo[i] !== minusculo[texto - 1 - i]) {
-        return false;
-      }
-      return true;
+  let minusculo = string.replace(/\s/g, "").toLowerCase();
+  let texto = minusculo.length;
+
+  for (i = 0; i < texto; i++) {
+    if (minusculo[i] !== minusculo[texto - 1 - i]) {
+      return false;
     }
   }
-  isPalindrome("Radar");
-  
-  function arrayMaxMin(array) {
-    let max = 0;
-    let min = 0;
-  
-    for (let i = 0; i < array.length; i++) {
-      let current = array[i];
-  
-      if (current > array[max]) {
-        max = i;
-      }
-      if (current < array[min]) {
-        min = i;
-      }
-      return (arrayMaxMin(max, min));
+  return true;
+}
+isPalindrome("A sacada da casa");
+
+function arrayMaxMin(array) {
+  let max = array[0];
+  let min = array[0];
+
+  for (let i = 0; i < array.length; i++) {
+    let current = array[i];
+
+    if (current > max) {
+      max = array[i];
+    } else if (current < min) {
+      min = array[i];
     }
   }
-  
-  console.log(arrayMaxMin[(3, 5, 6, 2, 1, 4)]);
+  return [min, max];
+}
+
+console.log(arrayMaxMin[3, 5, 6, 2, 1, 4];
