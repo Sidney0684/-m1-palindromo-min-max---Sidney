18bdede81275214ce51fc820f7d7cea3ce54cf8d
Sidney Lopes <sidney.silvalopes@gmail.com>
2024-05-22 23:49:02 -0300

    arrayMaxMin

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
-function isPalindrome(str){
-    /* Seu código aqui */
-}
-
-function arrayMaxMin(arr){
-    /* Seu código aqui */
-}
+function isPalindrome(string) {
+    let minusculo = string.toLowerCase().replace();
+    let texto = string.length;
+  
+    for (i = 0; i < texto; i++) {
+      if (minusculo[i] !== minusculo[texto - 1 - i]) {
+        return false;
+      }
+      return true;
+    }
+  }
+  isPalindrome("Radar");
+  
+  function arrayMaxMin(array) {
+    let max = 0;
+    let min = 0;
+  
+    for (let i = 0; i < array.length; i++) {
+      let current = array[i];
+  
+      if (current > array[max]) {
+        max = i;
+      }
+      if (current < array[min]) {
+        min = i;
+      }
+      return (arrayMaxMin(max, min));
+    }
+  }
+  
+  console.log(arrayMaxMin[(3, 5, 6, 2, 1, 4)]);
